678395056a005a95597886750e7ae7bacb894b8b
Adam Kullberg <a.kullberg@easternbank.com>
2020-02-17 08:18:24 -0500

    Using express makes it work now.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import ApplicationModule from './ApplicationModule';
 
 /**
  * The entry point for the Nest microservice.
  */
 async function bootstrap(): Promise<void> {
-  // Disable logging for the FastifyAdapter and use console as the logger for
-  // the whole application to disable the color (useful when deploying to Apigee).
-  const app = await NestFactory.create<NestFastifyApplication>(
-    ApplicationModule,
-    new FastifyAdapter({ logger: false }),
-    { logger: console }
-  );
-
-  await app.listen(3000, '0.0.0.0');
+  const app = await NestFactory.create(ApplicationModule);
+  await app.listen(3000);
 }
 
 // Start the microservice.
